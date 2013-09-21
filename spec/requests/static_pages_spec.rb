0d7c40803109cabbc6d2738aaa90e883ae00e0b1
JIuct <dgoul11@gmail.com>
2013-09-21 15:10:52 +0300

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,12 +2,20 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:title) { "Ruby on Rails Tutorial Sample App | " }
+
+######################################
   describe "Home page" do
 
-	it "should have the right title" do
-	  visit '/static_pages/home'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-	end
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/help'
+      expect(page).not_to have_title('| Home')
+    end
 
 
     it "should have the content 'Sample App'" do
@@ -16,26 +24,33 @@ describe "Static pages" do
     end
   end
 
+######################################
   describe "Help page" do
-
-	it "should have the right title" do
-	  visit '/static_pages/help'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-	end
-
-    it "should have the content 'Help'" do
+    it "should have the content 'Sample App'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{title}Help")
+    end
   end
 
+######################################
+  describe "Contact page" do
+  	it "should have the right title" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_title("#{title}Contact")
+  	end
+  end
 
+######################################
   describe "About page" do
-
-	it "should have the right title" do
-	  visit '/static_pages/about'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
-	end
+  	it "should have the right title" do
+  	  visit '/static_pages/about'
+  	  expect(page).to have_title("#{title}About")
+  	end
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
