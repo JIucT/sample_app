1e65cafff42cef45f2a80500b144edf60354f77c
JIuct <dgoul11@gmail.com>
2013-10-04 22:12:03 +0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+
+  resources :users
+
+#  get "users/new"
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
